f8e4b5ca019d53ecc9ae4e56d173bac5a0391b78
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2024-09-22 13:14:12 +0300

    Picture loader api

---
+++ src/test/java/com/example/picture_loader_api/PictureLoaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picture_loader_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureLoaderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

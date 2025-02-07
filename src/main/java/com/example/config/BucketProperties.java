a05afda99965e711a339cdeefcfeda75a2a53e7e
Sergey Kurochkin <serega.curo4kin2014@@gmail.com>
2025-02-07 14:39:03 +0300

    fixed bugs

--- src/main/java/com/example/config/BucketProperties.java
+++ src/main/java/com/example/config/BucketProperties.java
@@ -2,8 +2,8 @@ package com.example.config;
 
 public class BucketProperties {
     public static String BUCKET_NAME = "image-loader";
-    public static String ACCESS_KEY_ID = "YCAJEQc64EqqLU5ALml3AT6YJ";
-    public static String SECRET_ACCESS_KEY = "YCOl6hGrq4WVHPV0LfkXmUKqHtyBZaZfWQthWtTN";
+    public static String ACCESS_KEY_ID = "YCAJE4sIv1U5fqhPftf7LjGfd";
+    public static String SECRET_ACCESS_KEY = "YCPC4PSCM3U0dcAcpZbBYtSVWZ-Q8QpNvsBtCAuH";
 
     public static final String STORAGE_PATH = "https://storage.yandexcloud.net/";
 }
